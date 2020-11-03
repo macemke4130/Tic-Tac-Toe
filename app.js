7448d2e7124f1f2f6c60c0ef37e9ab4910fd1983
Lucas Mace <lucasmace4130@gmail.com>
2020-11-02 21:55:43 -0600

    Added a Flipper function

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ for (let index = 0; index < cells.length; index++) {
     cells[index].addEventListener('click', cellClicked);
 }
 
-function cellClicked() {
-   event.target.textContent = "X";
+let player = "X"; // Sets initial player to X --
+function flipper() {
+    if (player == "X") {
+        event.target.textContent = player;
+        player = "O";
+    } else if (player == "O") {
+        event.target.textContent = player;
+        player = "X";
+    }
 }
+
+function cellClicked() {
+    flipper();
+}
+
